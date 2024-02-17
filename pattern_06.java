31a1af0f1a1d14b20e1367f01e6d085a349152a3
gitdemo <135804234+ExplorerSoul@users.noreply.github.com>
2024-02-17 15:42:08 +0530

    pattern

---
+++ pattern_06.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+public class pattern_06 {
+    /*
+    Time Complexity : O(N^2)
+    Space complexity: O(1)
+
+    Where N is the given input.
+*/
+
+
+    static void pattern(int n){
+        
+        // For loop 'row' in range 0 to N-1.
+        for(int row = 0; row < n; row++)
+        {
+            // For loop 'col' in range 0 to row.
+            for(int col = 0; col < n; col++)
+            {
+                // If 'col' < N-'row', print  a '*'.
+                if(col < (n-row))
+                {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        pattern(n);
+        scanner.close();
+    }
+}
+
