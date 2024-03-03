cd3e117288df0e7904d02d2bb866c18814cab4ca
gitdemo <135804234+ExplorerSoul@users.noreply.github.com>
2024-03-03 15:09:25 +0530

    arrays

---
+++ arrays_swap.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class arrays_swap {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        swap(arr, 1, 3);
+
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
