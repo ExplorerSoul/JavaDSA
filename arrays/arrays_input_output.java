184594f717360f3e18658cd583adb3f70625a70e
gitdemo <135804234+ExplorerSoul@users.noreply.github.com>
2024-03-17 23:41:19 +0530

    i/p and o/p
    take input from user and print it

---
+++ arrays/arrays_input_output.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class arrays_input_output {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter " +n+ " array element: ");
+        for(int i = 0 ; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Elements in array are: ");
+        for(int i=0; i<n; i++){
+            if(i==0){
+                System.out.print("[ ");
+            }
+            System.out.print(arr[i] + " ");
+            if(i==n-1){
+                System.out.print("]");
+            }
+        }
+    }
+}
