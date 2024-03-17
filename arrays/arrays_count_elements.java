74ecf2f1e759400b32c27ceef38a67b468f141f3
gitdemo <135804234+ExplorerSoul@users.noreply.github.com>
2024-03-18 00:16:14 +0530

    elements
    number of elements in array

---
+++ arrays/arrays_count_elements.java
@@ -0,0 +1,6 @@
+public class arrays_count_elements {
+    public static void main(String[] args) {
+        int[] arr = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+        System.out.println("Total elements in array are : " + arr.length);
+    }
+}
