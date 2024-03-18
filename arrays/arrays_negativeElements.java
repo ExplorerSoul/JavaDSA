685a946e9c9d8b3b9693c0609e6085f95a427f48
gitdemo <135804234+ExplorerSoul@users.noreply.github.com>
2024-03-18 15:59:01 +0530

    Update arrays_negativeElements.java

--- arrays/arrays_negativeElements.java
+++ arrays/arrays_negativeElements.java
@@ -5,18 +5,26 @@ public class arrays_negativeElements {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size of array: ");
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        // int[] arr = new int[n];
+        Double[] arr1 = new Double[n];
         System.out.println("Enter elements: ");
         for(int i=0; i<n; i++){
-            arr[i] = sc.nextInt();
+            arr1[i] = sc.nextDouble();
+            // arr[i] = sc.nextInt();
         }
         int negativeCount = 0;
-        for(int i=0; i<n; i++){
-            if(arr[i]<0){
+        // for(int i=0; i<n; i++){
+        //     if(arr[i]<0){
+        //         negativeCount++;
+        //     }
+        // }
+        for(int i=0; i<arr1.length; i++){
+            if(Math.signum(arr1[i])== -1.0){
                 negativeCount++;
             }
-            
         }
         System.out.println("Total negative numbers are: " + negativeCount);
+
+        sc.close();
     }
 }
