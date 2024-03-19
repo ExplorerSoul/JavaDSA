7747fc2668f7445287e9790bc0d2035df2425e73
gitdemo <135804234+ExplorerSoul@users.noreply.github.com>
2024-03-19 08:11:45 +0530

    sum elements

--- arrays/array_sum_elements.java
+++ arrays/array_sum_elements.java
@@ -8,11 +8,9 @@ public class array_sum_elements {
         int n = sc.nextInt();
         int[] arr = new int[n];
         System.out.print("Enter " +n+ " elements:");
-        for(int i=0; i<n; i++){
-            arr[i] = sc.nextInt();
-        }
         int sum = 0;
         for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
             sum = sum + arr[i];
         }
         System.out.println("Sum of all elements is: " +sum);
