55106a4dff5a23e394c730a25cc894661679ba6b
gitdemo <135804234+ExplorerSoul@users.noreply.github.com>
2024-03-23 12:16:59 +0530

    linked list

--- arrays/array_insert.java
+++ arrays/array_insert.java
@@ -13,9 +13,17 @@ public class array_insert {
         System.out.print("Enter the element to insert at the end of the array: ");
         int element = sc.nextInt();
         arr[n] = element;
-        for (int j = 0; j < arr.length; j++) {
-            System.out.print(arr[j] + " ");
-        }
+        insertAtEnd(8);
         sc.close();
     }
+}
+
+static int insertAtEnd(int e){
+    int n = 5;
+    int[] array = new int[n+1];
+    array[n] = e;
+    for (int j = 0; j < array.length; j++) {
+        System.out.print(array[j] + " ");
+    }
+    return insertAtEnd(e);
 }
---
+++ linkedList/Main.java
@@ -0,0 +1,13 @@
+package linkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(99);
+        list.display();
+    }
+}
